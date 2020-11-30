7ab59f417370c0b0cde69454596794f03b366250
Austin Griffith <austin@concurrence.io>
2020-11-30 12:04:16 -0700

    progress

--- prysm/prysm.js
+++ prysm/prysm.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 
 console.log("Starting up prysm node...")
 try{
-  exec('./prysm.sh beacon-chain --accept-terms-of-use --http-web3provider=../geth/mainnet_data/geth.ipc >> ./prysm.log 2>&1', (err, stdout, stderr) => {
+  exec('./prysm.sh beacon-chain --accept-terms-of-use --http-web3provider=http://0.0.0.0:8545 >> ./prysm.log 2>&1', (err, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
   })
